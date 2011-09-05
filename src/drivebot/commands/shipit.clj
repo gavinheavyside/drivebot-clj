e6d263e88ae344ad0332406cb90f95018c9c80ec
Gavin Heavyside <gavin@heavyside.co.uk>
2011-09-05 11:41:19 +0100

    case-insensitive ship it

--- src/drivebot/commands/shipit.clj
+++ src/drivebot/commands/shipit.clj
@@ -1,13 +1,14 @@
- (ns drivebot.commands.shipit
-   (:require [drivebot.campfire :as cf]
-             [robert.hooke :as rh]))
+(ns drivebot.commands.shipit
+  (:require [drivebot.campfire :as cf]
+            [robert.hooke :as rh]
+            [clojure.string :as s]))
 
 (def squirrel-uris ["http://shipitsquirrel.github.com/images/ship%20it%20squirrel.png"
                     "http://shipitsquirrel.github.com/images/squirrel.png"
                     "http://img.skitch.com/20100714-d6q52xajfh4cimxr3888yb77ru.jpg"])
 
 (defn shipit-hook [f message]
-  (if (re-find #"ship it" message)
+  (if (re-find #"ship it" (s/lower-case message))
       (cf/send-message (rand-nth squirrel-uris))
       (f message)))
 
