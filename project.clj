1fbcf341c1d173933f7ce68e5bc8238e13d55369
Gavin Heavyside <gavin@heavyside.co.uk>
2011-09-08 12:24:22 +0100

    not using match any more

--- project.clj
+++ project.clj
@@ -3,6 +3,5 @@
   :dependencies [[org.clojure/clojure "1.2.1"]
                  [http.async.client "0.3.1"]
                  [org.danlarkin/clojure-json "1.2-SNAPSHOT"]
-                 [match "0.2.0-SNAPSHOT"]
                  [robert/hooke "1.1.2"]]
   :main drivebot.core)
